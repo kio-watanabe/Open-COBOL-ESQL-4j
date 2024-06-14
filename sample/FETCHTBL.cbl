736a26deb8098dbd3d07e8d4d268b5648e9b9000
kio-watanabe <ki-watanabe@tsh-world.co.jp>
2024-06-14 17:28:00 +0900

    Debug

--- sample/FETCHTBL.cbl
+++ sample/FETCHTBL.cbl
@@ -48,7 +48,7 @@
       *    CONNECT
            MOVE  "testdb"          TO   DBNAME.
            MOVE  "postgres"        TO   USERNAME.
-           MOVE  "user"             TO   PASSWD.
+           MOVE  SPACE             TO   PASSWD.
            EXEC SQL
                CONNECT :USERNAME IDENTIFIED BY :PASSWD USING :DBNAME 
            END-EXEC.
