6435077f93fc514edf92f6ff958fbb5ceab679f6
kio-watanabe <ki-watanabe@tsh-world.co.jp>
2025-08-05 06:57:37 +0000

    bug fix for vayring variables

--- dblibj/src/main/scala/Main.scala
+++ dblibj/src/main/scala/Main.scala
@@ -784,25 +784,14 @@ class OCESQLCursorFetchOne extends CobolRunnableWrapper {
         if (i < fields) {
           fetchRecord = fetchRecord ::: List(ocdbGetValue(rs, sv, i + 1))
           val byteArray: Array[Byte] = fetchRecord.flatten.flatten.toArray
-          println("dbg: type=" + sv.sqlVarType)
-          // if(sv.sqlVarType == OCDB_TYPE_ALPHANUMERIC_VARYING){
-          createCobolData(sv, 0, byteArray, state.globalState.occursInfo)
-          // }
+          if (
+            sv.sqlVarType == OCDB_TYPE_ALPHANUMERIC_VARYING || sv.sqlVarType == OCDB_TYPE_JAPANESE_VARYING
+          ) {
+            createCobolData(sv, 0, byteArray, state.globalState.occursInfo)
+          }
         }
       }
       fetchRecords = fetchRecords ::: List(fetchRecord)
-      // dbg-start
-      // val readableRecord = fetchRecord.map { recordOption =>
-      //     recordOption match {
-      //         // データが存在する場合 (Some)
-      //         case Some(bytes) => new String(bytes, "UTF-8") // バイト配列をUTF-8文字列に変換
-      //         // データが存在しない場合 (None / SQLのNULLに相当)
-      //         case None        => "NULL"
-      //     }
-      // }.mkString(", ") // 各要素をカンマ区切りで連結
-
-      // println("dbg:" + readableRecord)
-      // dbg-end
     }
     updateFetchRecords(name, fetchRecords, true, state)
   }
